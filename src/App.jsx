db7512a56d7e6c821520cf9d59ca9313ce3eaeea
amirsuhel88 <60836385+amirsuhel88@users.noreply.github.com>
2024-07-19 17:05:06 +0530

    update/edit todo working

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Users from "./components/Users";
 import Todos from "./components/Todos";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import CreateTodo from "./components/CreateTodo";
+import UpdateTodo from "./components/UpdateTodo";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/users" element={<Users />}></Route>
         <Route path="/" element={<Todos />}></Route>
         <Route path="/new-todo" element={<CreateTodo />}></Route>
+        <Route path="/edit-todo/:id" element={<UpdateTodo />}></Route>
       </Routes>
     </BrowserRouter>
   );
